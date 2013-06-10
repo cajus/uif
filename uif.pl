2ee8df90717b6986e553228e1e24b8b6ac77e62c
Mike Gabriel <mike.gabriel@das-netzwerkteam.de>
2013-06-11 00:26:50 +0200

    convert Umlaut to UTF-8, fix FSF address

--- uif.pl
+++ uif.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 
-# Copyright (C) 2002  J÷rg Platte <joergplatte@gmx.de>
+# Copyright (C) 2002  Jörg Platte <joergplatte@gmx.de>
 #                 and Cajus Pollmeier <pollmeier@gonicus.de>
 #
 # This program is free software; you can redistribute it and/or modify
@@ -15,7 +15,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 #
 # On Debian GNU/Linux systems, a copy of the GNU General Public License may be
 # found in the file /usr/share/common-licenses/GPL.
